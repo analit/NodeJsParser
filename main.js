de8637b590a9d681c392c56dfb5725efd5875b5c
Sergei <you@example.com>
2016-08-12 18:40:43 +0300

    job 12.08

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 var request = require('request');
-var fs = require('fs');
+
 var xmlBuilder = require('xmlbuilder');
 
 
-//var url = "http://1582.com.ua/STROYMATERIALI.-STROITELNIE-MATERIALI/Besedki/Kovannie-besedki/22-1264-1632-0.html";
-var url = "http://localhost/NodeJsParser/test.html";
+var url = "http://1582.com.ua/STROYMATERIALI.-STROITELNIE-MATERIALI/Besedki/Kovannie-besedki/22-1264-1632-0.html";
+// var url = "http://localhost/NodeJsParser/test.html";
 
 var Iconv = require('iconv').Iconv;
 var fromEnc = 'cp1251';
@@ -18,30 +18,32 @@ request({url: url, encoding: null}, function (error, response, body) {
 });
 
 
-function createKindFile(error, response, body) {
+// function createKindFile(error, response, body) {
 
-    console.log(translator.convert(body).toString());
-    process.exit(1);
-    
-    $ = cheerio.load(response);
-    var listFirms = $("table[width='97%'] tr");
-    
     
 
-    var firms = xmlBuilder.create("firms");
-    var phones = firms.ele('firm', {name: "Test firm name"}).
-            ele('address', {address: "Симферополь, ул. Ветлицкого, 25, кв. 18"}).
-            ele('phones');
-
-    phones.ele('phone', {}, "25-12-49");
-    phones.ele('phone', {}, "25-12-48");
-
-    fs.writeFile('kinds-data/blya.xml', firms);
-}
-
-function appendFirm(){
+//     console.log(translator.convert(body).toString());
+//     process.exit(1);
     
-}
+//     $ = cheerio.load(response);
+//     var listFirms = $("table[width='97%'] tr");
+    
+    
+
+//     var firms = xmlBuilder.create("firms");
+//     var phones = firms.ele('firm', {name: "Test firm name"}).
+//             ele('address', {address: "Симферополь, ул. Ветлицкого, 25, кв. 18"}).
+//             ele('phones');
+
+//     phones.ele('phone', {}, "25-12-49");
+//     phones.ele('phone', {}, "25-12-48");
+
+//     fs.writeFile('kinds-data/blya.xml', firms);
+// }
+
+// function appendFirm(){
+    
+// }
 
 
 
--- modules/save-parse-data.js
+++ modules/save-parse-data.js
@@ -1,5 +1,6 @@
 var cheerio = require('cheerio');
 var xmlBuilder = require('xmlbuilder');
+var fs = require('fs');
 
 function isCheck(phones, address) {
     if (phones.text().match(/\+7\(978\)/)) {
@@ -10,18 +11,26 @@ function isCheck(phones, address) {
         return true;
     }
 
-    return false
+    return false;
+}
+
+function getFirmId(firmUrl){
+    var codes = firmUrl.match(/([0-9]+-){5}[0-9]+/);
+    return codes[0].split("-")[3];
 }
 
 function appendDataToFirms(name, phones, address) {
-    var firm = this.ele('firm', {name: name.find("span").text().trim()});
-    var address = address.text().split("\n");
-    var address = firm.ele('address', {address: address[1].trim(), region:address[2].trim()})
-    console.log();
+    var firmNode = this.ele('firm', {name: name.find("span").text().trim(), id:getFirmId(name.find("a").attr("href"))});
+    var addressToArray = address.text().split("\n");
+    var addressNode = firmNode.ele('address', {address: addressToArray[1].trim(), region:addressToArray[2].trim()})
+    var phonesNode = addressNode.ele('phones');
+    phones.html().split("<br>").forEach(function (element) {
+       element.trim().length && phonesNode.ele('phone', {}, element.trim())
+    });
 }
 
 function getFileName(url) {
-    return "ppc.xml";
+    return url.match(/([0-9]+-){2,3}[0-9]+/)[0];
 }
 
 module.exports.saveToXml = function (url, body) {
@@ -34,7 +43,8 @@ module.exports.saveToXml = function (url, body) {
         isCheck(tds.eq(2), tds.eq(3)) && appendDataToFirms.call(firms, tds.eq(1), tds.eq(2), tds.eq(3));
     });
 
-    console.log(firms.toString());
+    // console.log(firms.toString());
+    fs.writeFile('kinds-data/' + getFileName(url), firms);
 };
 
 
