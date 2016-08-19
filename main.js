36366da9487c3d3076e79e97000d0418e5d21eb3
Sergei <you@example.com>
2016-08-19 18:48:27 +0300

    job 18.08

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 var request = require('request');
 var fs = require('fs');
-var xmlBuilder = require('xmlbuilder');
 
 var url = "http://1582.com.ua/STROYMATERIALI.-STROITELNIE-MATERIALI/Besedki/Kovannie-besedki/22-1264-1632-0.html";
+var url = "http://localhost/NodeJsParser/test2.html";
 
 var Iconv = require('iconv').Iconv;
 var fromEnc = 'cp1251';
@@ -10,16 +10,18 @@ var toEnc = 'utf-8';
 var translator = new Iconv(fromEnc, toEnc);
 
 function getFileName(url) {
-    return url.match(/([0-9]+-){2,3}[0-9]+/)[0];
+    var match = url.match(/([0-9]+-){2,3}[0-9]+/);
+    if (match) {
+        return match[0];
+    }
+    return 'test';
 }
 
 var fileName = getFileName(url) + ".xml";
 
-fs.access('kinds-data/blya.xml', null, (err) => {
-    console.log('File %s already exists!');
-});
 
 request({url: url, encoding: null}, function (error, response, body) {
     var xmlData = require('./modules/save-parse-data').getXmlData(translator.convert(body).toString());
+    xmlData.att("url", url);
     fs.writeFile('kinds-data/' + getFileName(url) + ".xml", xmlData);
 });
--- modules/save-parse-data.js
+++ modules/save-parse-data.js
@@ -19,13 +19,27 @@ function getFirmId(firmUrl){
     return codes[0].split("-")[3];
 }
 
-function appendDataToFirms(name, phones, address) {
-    var firmNode = this.ele('firm', {name: name.find("span").text().trim(), id:getFirmId(name.find("a").attr("href"))});
-    var addressToArray = address.text().split("\n");
-    var addressNode = firmNode.ele('address', {address: addressToArray[1].trim(), region:addressToArray[2].trim()})
+function getUrlLogo(tdLogo) {
+    if (tdLogo.find("img").length){
+        return tdLogo.find("img").attr("src")
+    }
+    return "";
+}
+
+function appendDataToFirms(tdLogo, tdName, tdPhones, tdAddress) {
+    var firmAttributes = {
+        name: tdName.find("span").text().trim(),
+        id: getFirmId(tdName.find("a").attr("href")),
+        path: tdName.find("a").attr("href"),
+        logo: getUrlLogo(tdLogo)
+    };
+
+    var firmNode = this.ele('firm', firmAttributes);
+    var addressToArray = tdAddress.text().split("\n");
+    var addressNode = firmNode.ele('address', {address: addressToArray[1].trim(), region: addressToArray[2].trim()})
     var phonesNode = addressNode.ele('phones');
-    phones.html().split("<br>").forEach(function (element) {
-       element.trim().length && phonesNode.ele('phone', {}, element.trim())
+    tdPhones.html().split("<br>").forEach(function (element) {
+        element.trim().length && phonesNode.ele('phone', {}, element.trim())
     });
 }
 
@@ -37,11 +51,10 @@ module.exports.getXmlData = function (body) {
 
     listFirms.each(function () {
         var tds = $(this).find("td");
-        isCheck(tds.eq(2), tds.eq(3)) && appendDataToFirms.call(firms, tds.eq(1), tds.eq(2), tds.eq(3));
+        isCheck(tds.eq(2), tds.eq(3)) && appendDataToFirms.call(firms, tds.eq(0), tds.eq(1), tds.eq(2), tds.eq(3));
     });
 
-    // console.log(firms.toString());
-    
+    return firms;
 };
 
 
